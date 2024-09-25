f24ab827116cc0dce4a173e1e9931b4b1933265f
Caleb <calebkiune@gmail.com>
2024-09-25 15:39:45 +0300

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1=2
+let num2=31
+let multiply=num1*num2;
+let random=Math.floor(Math.random() * 9);
+let num3=40
+let num4=6
+let mod=num3%num4;
+let max=Math.max(5,10,15,20)
 
-// Write your code here
+
+
+
